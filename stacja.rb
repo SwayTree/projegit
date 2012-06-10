f8dfa2f69fde35559688d881c12282cef9620b56
matiit <matiqing@gmail.com>
2012-06-10 17:13:39 +0200

    dodanie metody dodajPrzesylke do klasy Stacja

--- stacja.rb
+++ stacja.rb
@@ -1,3 +1,14 @@
 class Stacja
-    attr_accessor :id, :tablicaOdleglosci
+    attr_accessor :id, :tablicaOdleglosci, :przesylki
+    def initialize():
+        # Utworzenie zmiennej klasowej przesylki, typu Array (array moze przechowywac rzeczy roznych typow)
+        # indeksowane przez liczby oczywiscie
+        @przesylki = Array.new()
+    
+    # Dodanie przesylki do kolekcji przesylki    
+    def dodajPrzesylke(przesylka)
+        @przesylki << przesylka 
+        # Lub @przesylki.push(przesylka) nie wiem, ktory sposob fajniejszy, pasowaloby sie trzymac jednego
+    end
 end
+
